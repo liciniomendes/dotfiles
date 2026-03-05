534ba0f5bd99cd091029c979e1315cb67c1126b7
Licínio Mendes <licinioamendes@gmail.com>
2026-03-05 22:30:08 +0000

    added snacks picker

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,23 @@
 --
 -- My specific keymaps
--- 
+--
 
 local map = vim.keymap.set
 
 -- Buffers
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Previous buffer' })
+map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+
+-- Snacks
+map('n', '<leader>ff', function()
+  Snacks.picker.files()
+end)
+map('n', '<leader>fg', function()
+  Snacks.picker.grep()
+end)
+map('n', '<leader>fb', function()
+  Snacks.picler.buffers()
+end)
+map('n', '<leader>fs', function()
+  Snacks.picker.lsp_symbols()
+end)
