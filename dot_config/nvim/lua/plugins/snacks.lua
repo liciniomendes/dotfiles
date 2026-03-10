eac165519ba4c7bc23ddb997efa6fd4b45600d54
Licínio Mendes <licinioamendes@gmail.com>
2026-03-10 16:00:05 +0000

    enable snacks explorer and keybinds

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,10 +1,24 @@
-return {
-  'folke/snacks.nvim',
-  priority = 1000,
-  lazy = false,
   opts = {
+    explorer = {
+      replace_netrw = true,
+      enabled = true,
+      -- Auto-jump to current file
+      follow_file = false,
+      -- Show dotfiles
+      hidden = true,
+      -- Show git changes in tree
+      git_status = true,
+      -- Show untracked git files
+      git_untracked = true,
+      -- Show LSP diagnostics in the tree
+      diagnostics = true,
+      -- Enable fs watching (off by default)
+      watch = false,
+    },
     picker = {
       enabled = true,
+      -- This enables the picker show dotfiles by default
+      hidden = true,
     },
   },
 }
